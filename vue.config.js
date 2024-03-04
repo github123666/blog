7175c2204c84a72d45b91acdfff4e11e13dbedb6
rsp <3288892500@qq.com>
2024-03-04 17:16:50 +0800

    update unpack

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
+const { options } = require('marked')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false,
+  lintOnSave: false,
   publicPath: process.env.NODE_ENV === 'production'
-  ? './'
-  : './',
+    ? './'
+    : './',
   // assetsDir:'static',
+  // 关闭名称带hash
+  filenameHashing: false,
   //代理
-  devServer:{
+  devServer: {
     proxy: {
-      "api":{
-        target:'http://127.0.0.1:8000/',
-        changOrigin:true,
-        pathRewrite:{
-          '^/api':"/"
+      "api": {
+        target: 'http://127.0.0.1:8000/',
+        changOrigin: true,
+        pathRewrite: {
+          '^/api': "/"
         }
       }
     }
-  }
+  },
 })
